c7b281c60f193391833bc840a9d956c372f01c7e
cruzin4bruzinrl-code <cruzin4bruzin.rl@gmail.com>
2026-09-16 02:09:41 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.robapps.wallpaperhub"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.robapps.wallpaperhub"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
@@ -31,8 +31,8 @@ android {
 }
 
 dependencies {
-implementation("androidx.media3:media3-exoplayer:1.4.1")
-implementation("androidx.media3:media3-common:1.4.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-common:1.4.1")
 }
